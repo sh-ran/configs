034faa7eab77bbc04ca960d965fc5de20aa76183
Sharan <sharancodes@gmail.com>
2025-12-24 04:34:04 +0000

    rustfmt sp

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,9 +5,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+-- vim.cmd("colorscheme github_dark_default")
 vim.cmd("colorscheme rose-pine")
 require("config.keymaps")
 vim.cmd("set rnu")
+vim.cmd("let g:rustfmt_autosave=1")
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.writebackup = false
